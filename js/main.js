01704a3095627d1bc411183084e8e96cb1643b37
hwangina <hina5093@naver.com>
2019-06-18 10:48:28 +0900

    upload 2019-06-18

---
+++ js/main.js
@@ -0,0 +1,36 @@
+﻿window.addEventListener("load", function(){
+	var handle=document.querySelector(".handle"); // active handler
+	var picture=document.querySelector(".picture ul"); // add in
+	var appendHTML=""; // for add li element innerText
+	
+	/* load json data */
+	var requestURL="data/img_path.json"; 
+	var request=new XMLHttpRequest();
+
+	function init(){
+		setTimeout(function(){
+			request.open("GET", requestURL, true);
+			request.responseType="json";
+			request.send();
+			
+			request.addEventListener("load", function(){
+				var data=request.response;
+				console.log(data);
+
+				for(key in data){
+					// console.log(key+" : "+data[key]);
+					// <li><img src="images/work1.jpg" alt=""></li>
+					appendHTML+='<li><img src="images/'+data[key]+'" alt=""></li>'+'\n';
+				}
+				// console.log("appendHTML : " + appendHTML);
+				picture.innerHTML=appendHTML;
+
+				handle.addEventListener("click", function(e){
+					e.preventDefault();
+					picture.classList.toggle("on");
+				});
+			});
+		}, 10);
+	}
+	init();
+});
